0b88db130ac1556da310e91b800a97a5fe30ecd7
Mamadou Sissoko <msissoko2317@gmail.com>
2025-11-02 20:00:48 -0800

    Milestone 5

--- 210-lab-28.cpp
+++ 210-lab-28.cpp
@@ -48,7 +48,7 @@ int main() {
     
     // Goat Manager 3001 Engine
     int sel = main_menu();
-    while (sel != 8) {
+    while (sel != 9) {
         switch (sel) {
             case 1:
                 cout << "Adding a goat.\n";
@@ -95,7 +95,12 @@ int main() {
             default:
                 cout << "Invalid selection.\n";
                 break;
+            case 8:
+                cout << "There are currently " << trip.size() << " goats in the trip.\n";
+                break;
+            
         }
+
         sel = main_menu();
     }
     
@@ -112,11 +117,12 @@ int main_menu() {
     cout << "[5] Sorts goats in reverse order\n";
     cout << "[6] Display the first goat\n";
     cout << "[7] Display the last goat\n";
-    cout << "[8] Quit\n";
+    cout << "[8] Display goat total in list\n";
+    cout << "[9] Quit\n";
     cout << "Choice --> ";
     int choice;
     cin >> choice;
-    while (choice < 1 || choice > 8) {
+    while (choice < 1 || choice > 9) {
         cout << "Invalid, again --> ";
         cin >> choice;
     }
